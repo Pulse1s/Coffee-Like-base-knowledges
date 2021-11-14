1afb5b410dae1b37da3ac6821e2d2a14e114480d
Pulse <poradoks7@gmail.com>
2021-11-14 19:22:31 +0300

    fix userlist

--- resources/views/livewire/userlist.blade.php
+++ resources/views/livewire/userlist.blade.php
@@ -1,4 +1,4 @@
-<div style="overflow-x: auto;">
+<div>
 
     <div class="d-flex justify-content-center my-3">
         <div class="col-8">
@@ -6,56 +6,58 @@
         </div>
     </div>
 
-    <table class="table table-hover">
-        <thead>
-        <tr>
-            <th scope="col">
-                <button class="btn py-0 fw-bold" wire:click="orderById">id</button>
-            </th>
-            <th scope="col">
-                <button class="btn py-0 fw-bold" wire:click="orderByFirstName">Имя</button>
-            </th>
-            <th scope="col">
-                <button class="btn py-0 fw-bold" wire:click="orderByLastName">Фамилия</button>
-            </th>
-            <th scope="col">
-                <button class="btn py-0 fw-bold" wire:click="orderByPhone">Номер</button>
-            </th>
-            <th scope="col">
-                <button class="btn py-0 fw-bold" wire:click="orderByEmail">Почта</button>
-            </th>
-            <th scope="col">
-                <button class="btn py-0 fw-bold" wire:click="orderByActive">Активен</button>
-            </th>
-        </tr>
-        </thead>
-        <tbody>
-
-
-        @foreach($users->filter(
-        function ($value, $key) use($search) {
-            if($search)
-                return str_contains($value['first_name'], mb_convert_case($search, MB_CASE_TITLE, "UTF-8"))
-                or str_contains($value['last_name'], mb_convert_case($search, MB_CASE_TITLE, "UTF-8"));
-            else
-                return $value;
-             }) as $user)
-
-            <tr class="@if($user->active) table-success @else table-danger @endif">
-                <td>{{ $user->id }}</td>
-                <td>{{ $user->first_name }}</td>
-                <td>{{ $user->last_name }}</td>
-                <td>+7{{ $user->phone }}</td>
-                <td>{{ $user->email }}</td>
-                <td>@if($user->admin) <span class="text-danger">[adm]</span> @else
-                        <button class="btn py-0 border- border-secondary"
-                                type="button" wire:click="activate({{ $user->id }})">{{ $user->active ? 'Да' : 'Нет' }}</button>
-                    @endif </td>
+    <div style="overflow-x: auto;">
+        <table class="table table-hover">
+            <thead>
+            <tr>
+                <th scope="col">
+                    <button class="btn py-0 fw-bold" wire:click="orderById">id</button>
+                </th>
+                <th scope="col">
+                    <button class="btn py-0 fw-bold" wire:click="orderByFirstName">Имя</button>
+                </th>
+                <th scope="col">
+                    <button class="btn py-0 fw-bold" wire:click="orderByLastName">Фамилия</button>
+                </th>
+                <th scope="col">
+                    <button class="btn py-0 fw-bold" wire:click="orderByPhone">Номер</button>
+                </th>
+                <th scope="col">
+                    <button class="btn py-0 fw-bold" wire:click="orderByEmail">Почта</button>
+                </th>
+                <th scope="col">
+                    <button class="btn py-0 fw-bold" wire:click="orderByActive">Активен</button>
+                </th>
             </tr>
-        @endforeach
+            </thead>
+            <tbody>
 
 
-        </tbody>
-    </table>
+            @foreach($users->filter(
+            function ($value, $key) use($search) {
+                if($search)
+                    return str_contains($value['first_name'], mb_convert_case($search, MB_CASE_TITLE, "UTF-8"))
+                    or str_contains($value['last_name'], mb_convert_case($search, MB_CASE_TITLE, "UTF-8"));
+                else
+                    return $value;
+                 }) as $user)
+
+                <tr class="text-nowrap @if($user->active) table-success @else table-danger @endif">
+                    <td>{{ $user->id }}</td>
+                    <td>{{ $user->first_name }}</td>
+                    <td>{{ $user->last_name }}</td>
+                    <td>+7{{ $user->phone }}</td>
+                    <td>{{ $user->email }}</td>
+                    <td>@if($user->admin) <span class="text-danger">[adm]</span> @else
+                            <button class="btn py-0 border- border-secondary"
+                                    type="button" wire:click="activate({{ $user->id }})">{{ $user->active ? 'Да' : 'Нет' }}</button>
+                        @endif </td>
+                </tr>
+            @endforeach
+
+
+            </tbody>
+        </table>
+    </div>
 </div>
 
